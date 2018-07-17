f1a288829948c220dab4306b900a36d887b39c1a
Glen Pegado <glenpegado@gmail.com>
2018-07-17 16:41:39 -0700

    Final changes

--- variables.js
+++ variables.js
@@ -2,7 +2,8 @@ const inputs = document.querySelectorAll('.controls input')
 
 function handleUpdate() {
   const suffix = this.dataset.sizing || '';
-  console.log(this.blur)
+
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate))

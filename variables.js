111ca7e29b3432b066f9537cf745e744fa808b25
Glen Pegado <glenpegado@gmail.com>
2018-07-17 16:31:30 -0700

    changed hanldeUpdate function

--- variables.js
+++ variables.js
@@ -1,7 +1,9 @@
 const inputs = document.querySelectorAll('.controls input')
 
 function handleUpdate() {
-  console.log(this.value)
+  const suffix = this.dataset.sizing || '';
+  console.log(this.blur)
 }
 
-input.forEach(input => )
+inputs.forEach(input => input.addEventListener('change', handleUpdate))
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
